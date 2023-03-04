d1d2f9a3c796af5dafc3c51cb8c682ecd34136aa
codingllama7 <122734727+codingllama7@users.noreply.github.com>
2023-03-04 22:18:28 +0100

    corrections

--- Excersises/Easy/BallparkOrders.java
+++ Excersises/Easy/BallparkOrders.java
@@ -4,10 +4,13 @@ public class BallparkOrders {
     //Your code goes here
 }
 /*
-You and three friends go to a baseball game and you offer to go to the concession stand for everyone. They each order one thing, and you do as well. Nachos and Pizza both cost $6.00. A Cheeseburger meal costs $10. Water is $4.00 and Coke is $5.00. Tax is 7%.
+You and three friends go to a baseball game and you offer to go to the concession stand for everyone. They each order
+one thing, and you do as well. Nachos and Pizza both cost $6.00. A Cheeseburger meal costs $10. Water is $4.00 and Coke
+is $5.00. Tax is 7%.
 
 Task
-Determine the total cost of ordering four items from the concession stand. If one of your friend’s orders something that isn't on the menu, you will order a Coke for them instead.
+Determine the total cost of ordering four items from the concession stand. If one of your friend’s orders something that
+isn't on the menu, you will order a Coke for them instead.
 
 Input Format
 You are given a string of the four items that you've been asked to order that are separated by spaces.
