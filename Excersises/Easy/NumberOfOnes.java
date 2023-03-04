44fd707179034f045b0e6def83fa00c21c47ab0f
codingllama7 <122734727+codingllama7@users.noreply.github.com>
2023-03-04 19:30:26 +0100

    26 easy excersises in total

---
+++ Excersises/Easy/NumberOfOnes.java
@@ -0,0 +1,21 @@
+package Excersises.Easy;
+
+public class NumberOfOnes {
+    //Your code goes here
+}
+/*
+Task:
+Count the number of ones in the binary representation of a given integer.
+
+Input Format:
+An integer.
+
+Output Format:
+An integer representing the count of ones in the binary representation of the input.
+
+Sample Input:
+9
+
+Sample Output:
+2
+*/
