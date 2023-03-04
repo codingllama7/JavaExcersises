6c3d55d333716364aaebd08e6c32a60e35295790
codingllama7 <122734727+codingllama7@users.noreply.github.com>
2023-03-04 20:54:03 +0100

    11 hard excersises in total

---
+++ Excersises/Hard/DigitsOfPi.java
@@ -0,0 +1,24 @@
+package Excersises.Hard;
+
+public class DigitsOfPi {
+    //Your code goes here
+}
+/*
+The constant Pi is defined as the ratio of a circle's circumference to its diameter.
+Pi is an irrational number, meaning that it's digits never end or repeat in any known way.
+
+Task:
+Given an integer N as input, find and output the Nth decimal digit of Pi.
+
+Input Format:
+An integer: 0<N<1000
+
+Output Format:
+An integer, representing the Nth decimal digit of Pi.
+
+Sample Input:
+12
+
+Sample Output:
+9
+*/
---
+++ Excersises/Hard/LongestCommonSubstring.java
@@ -0,0 +1,22 @@
+package Excersises.Hard;
+
+public class LongestCommonSubstring {
+    //Your code goes here
+}
+/*
+Task:
+Given multiple words, you need to find the longest string that is a substring of all words.
+
+Input Format:
+A string of words, separated by spaces. The string can also contain numbers.
+
+Output Format:
+A string, representing the longest common substring.
+If there are multiple longest common substrings, output the smallest one in alphabetical order.
+
+Sample Input:
+SoloLearn Learning LearningIsFun Learnable
+
+Sample Output:
+Learn
+*/
