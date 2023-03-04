2de2f3370920b860b52b93f1b888d9a7f4459d25
codingllama7 <122734727+codingllama7@users.noreply.github.com>
2023-03-04 21:09:41 +0100

    MVP

---
+++ Solutions/Easy/Popsicles.java
@@ -0,0 +1,40 @@
+package Solutions.Easy;
+
+import java.util.Scanner;
+public class Popsicles {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many siblings you have?");
+        int siblings = scanner.nextInt();
+        System.out.println("Now, check in the box! How many popsicles you have in there?");
+        int popsicles = scanner.nextInt();
+
+        if (popsicles % siblings == 0) {
+            System.out.println("give away");
+        } else {
+            System.out.println("eat them yourself");
+        }
+    }
+}
+/*
+You have a box of popsicles and you want to give them all away to a group of brothers and sisters. If you have enough
+left in the box to give them each an even amount you should go for it! If not, they will fight over them, and you should
+eat them yourself later.
+
+Task
+Given the number of siblings that you are giving popsicles to, determine if you can give them each an even amount or if
+you shouldn't mention the popsicles at all.
+
+Input Format
+Two integer values, the first one represents the number of siblings, and the second one represents the number of
+popsicles that you have left in the box.
+
+Output Format
+A string that says 'give away' if you are giving them away, or 'eat them yourself' if you will be eating them yourself.
+
+Sample Input
+3 9
+
+Sample Output
+give away
+*/
