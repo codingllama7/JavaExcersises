33e043fb5ec5d076829aa569248525ed06fc1b4f
codingllama7 <122734727+codingllama7@users.noreply.github.com>
2023-03-07 18:37:19 +0100

    mvp

---
+++ Solutions/Easy/FruitBowl.java
@@ -0,0 +1,34 @@
+package Solutions.Easy;
+
+import java.util.Scanner;
+public class FruitBowl {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter an even number which represents the amount of fruits in the bowl:");
+        int totalFruits = scanner.nextInt();
+        int apples = totalFruits / 2;
+        int pies = apples / 3;
+
+        System.out.println(pies);
+    }
+}
+/*
+You have a bowl on your counter with an even number of pieces of fruit in it. Half of them are bananas, and the other
+half are apples. You need 3 apples to make a pie.
+
+Task
+Your task is to evaluate the total number of pies that you can make with the apples that are in your bowl given to total
+amount of fruit in the bowl.
+
+Input Format
+An integer that represents the total amount of fruit in the bowl.
+
+Output Format
+An integer representing the total number of whole apple pies that you can make.
+
+Sample Input
+26
+
+Sample Output
+4
+*/
